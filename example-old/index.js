e158804803299b5fb097a5933c67c6993b04a59b
Lue Hang <luehang17@gmail.com>
2019-12-28 15:07:10 -0600

    Remove old example.

--- example-old/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from "react-native";
-import ReactNativeSwipeRenderExample from "./ReactNativeSwipeRenderExample";
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent(appName, () => ReactNativeSwipeRenderExample);
